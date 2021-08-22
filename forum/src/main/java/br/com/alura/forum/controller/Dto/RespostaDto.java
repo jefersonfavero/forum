fe837714097d1c5ad1b215cba259638434254bc7
jeferson lestat <jefersonvieirafavero@gmail.com>
2021-08-22 20:03:28 -0300

    adicionando projeto forum com spring boot

---
+++ forum/src/main/java/br/com/alura/forum/controller/Dto/RespostaDto.java
@@ -0,0 +1,37 @@
+package br.com.alura.forum.controller.Dto;
+
+import java.time.LocalDateTime;
+
+import br.com.alura.forum.modelo.Resposta;
+
+public class RespostaDto {
+	private Long id;
+	private String mensagem;
+	private LocalDateTime dataCriação;
+	private String nomeAutor;
+	
+	public RespostaDto(Resposta resposta) {
+		this.id = resposta.getId();
+		this.mensagem = resposta.getMensagem();
+		this.dataCriação = resposta.getDataCriacao();
+		this.nomeAutor =  resposta.getAutor().getNome();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public LocalDateTime getDataCriação() {
+		return dataCriação;
+	}
+
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+	
+	
+}
